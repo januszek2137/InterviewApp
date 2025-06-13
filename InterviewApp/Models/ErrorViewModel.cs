e9cb92c8aa86f766b9e398897282339202254916
januszek2137 <mr.janolex@gmail.com>
2025-06-13 08:59:18 +0200

    Add project files.

---
+++ InterviewApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InterviewApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ InterviewApp/Models/PhoneModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InterviewApp.Models {
+    public class PhoneModel {
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Number { get; set; }
+    }
+}
